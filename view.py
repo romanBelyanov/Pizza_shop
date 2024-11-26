403afce75f79efaa88a83a43e16565a827eb1ffc
romanBelyanov <romkabel412@gmail.com>
2024-11-26 11:26:05 +0700

    Update view.py

--- view.py
+++ view.py
@@ -1,3 +1,5 @@
+import json
+from model import age_Check
 def hi():
     print("Здравствуйте! Выберите действие:")
     print("1 - Регистрация")
@@ -7,3 +9,16 @@ def hi():
         return 2
     else:
         return 1
+
+def show_menu(path):
+    dct = {"pizzas": "Пиццы:", "drinks": "Напитки: ", "alcohol": "Алкоголь: "}
+    with open(path, "r") as file:
+        res = json.load(file)
+    for food in res['menu']:
+        if food == 'alcohol' and not age_Check():
+            continue
+        print(dct[food])
+        for param in res['menu'][food]:
+            print(f"Название: {param['name']} Цена: {param['price']}")
+
+show_menu(r"C:\Users\Белянов Роман ИИ-71\PycharmProjects\pizza\menu.json")

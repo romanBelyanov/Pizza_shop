b7dae3971dd313affda920311719b3f2d8c7028c
romanBelyanov <romkabel412@gmail.com>
2025-04-14 19:53:15 +0700

    Delete view.py

--- view.py
+++
@@ -1,9 +0,0 @@
-def hi():
-    print("Здравствуйте! Выберите действие:")
-    print("1 - Регистрация")
-    print("2 - Авторизация")
-    ans = int(input())
-    if ans >= 2:
-        return 2
-    else:
-        return 1

af8a4b8d10e4465c48b1097320865fce62c09021
romanBelyanov <romkabel412@gmail.com>
2025-04-15 09:38:37 +0700

    Delete pizza_Django directory

--- pizza_Django/users/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class LoginUserForm(forms.Form):
-    username = forms.CharField(label="Логин",
-                               widget=forms.TextInput(attrs={'class': 'from-input'}))
-    password = forms.CharField(label="Пароль",
-                               widget=forms.PasswordInput(attrs={'class': 'from-input'}))
